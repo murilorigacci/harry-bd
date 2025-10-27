10ba65fd259ef977546c03ce32897a4ef3d6b95e
Murilo <murilo.r.rocha@aluno.senai.br>
2025-10-27 14:43:15 -0300

    Cria export do update, delete e post

--- src/models/bruxoModel.js
+++ src/models/bruxoModel.js
@@ -12,3 +12,36 @@ export const findById = async (id) => {
         where: { id: Number(id)}
     });
 }
+
+export const create = async (bruxo) => {
+    return await prisma.bruxo.create({
+        data: {
+            nome: data.nome,
+            casa: data.casa,
+            patrono:data,
+            varinha: data.varinha,
+            anoMatricula: data.anoMatricula.
+            anoMatricula
+        }
+    });
+}
+
+export const deleteBruxo = async (params) => {
+    return await prisma.bruxo.delete({
+        where: { id: Number(id)}
+    });
+}  
+
+export const update = async (id, data) => {
+    return await prisma.bruxo.update({
+        where: { id: Number(id) },
+        data: {
+            ...(data.nome && { nome: data.nome }),
+            ...(data.casa && { casa: data.casa }),
+            ...(data.patrono && { patrono: data.patrono }),
+            ...(data.varinha && { varinha: data.varinha }),
+            ...(data.anoMatricula && { anoMatricula: Number(data.anoMatricula) }),
+            ...(data.ativo !== undefined && { ativo: data.ativo }),
+        }
+    })
+}
--- src/routes/bruxoRoute.js
+++ src/routes/bruxoRoute.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get("/", BruxoController.listarTodos)
 router.get("/:id", BruxoController.listarUm)
+router.post("/", BruxoController.criar)
+router.delete("/:id", BruxoController.apagar)
+router.put("/:id", BruxoController.atualizar)
 
 export default router;
